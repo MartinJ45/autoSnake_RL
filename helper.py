04098e5e3fed75e0efa8f5a4eb3c817a14088896
mjimenez <bolitaycamilo45@gmail.com>
2023-04-27 16:03:17 -0400

    - Added a function to help display the game without the need of CMU_Graphics library

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from IPython import display
+import numpy as np
 
 plt.ion()
 
@@ -16,5 +17,17 @@ def plot(scores, mean_scores):
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+
+    plt.pause(0.001)
+
+
+def display_game(state):
+    np_grid = np.array(state)
+
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+    plt.imshow(np_grid)
+    plt.axis(False)
 
     plt.pause(0.001)
