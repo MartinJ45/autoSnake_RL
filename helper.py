36cb6ca417e1783e9c7b3026db911a594d033af1
mjimenez <bolitaycamilo45@gmail.com>
2023-05-01 15:59:30 -0400

    - Stopped outputs from printing when displaying the game

--- helper.py
+++ helper.py
@@ -2,18 +2,20 @@ import matplotlib.pyplot as plt
 from IPython import display
 import numpy as np
 
-plt.ion()
-
 
 def plot(scores, mean_scores):
+    plt.ion()
+
     display.clear_output(wait=True)
-    display.display(plt.gcf())
     plt.clf()
+
     plt.title('Training...')
     plt.xlabel('Number of Games')
     plt.ylabel('Score')
+
     plt.plot(scores)
     plt.plot(mean_scores)
+
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
@@ -22,12 +24,15 @@ def plot(scores, mean_scores):
 
 
 def display_game(state):
+    plt.ion()
+
     np_grid = np.array(state)
 
     display.clear_output(wait=True)
-    display.display(plt.gcf())
     plt.clf()
-    plt.imshow(np_grid)
+
     plt.axis(False)
+    plt.ioff()
+    plt.imshow(np_grid)
 
     plt.pause(0.001)
